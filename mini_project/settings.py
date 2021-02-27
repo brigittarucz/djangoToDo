59641c1df35f01dee915dd0ad7756d3a59636d40
Brigitta Rucz <43784818+brigittarucz@users.noreply.github.com>
2021-02-27 08:28:49 +0100

    Update settings.py

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3)$m3y$^6%t+0foth*hyvo=-imq6i02$95@)^(rj)!-fbgwj1$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
